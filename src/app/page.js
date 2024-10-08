b9a03d60a12d0952e044a54714853ac45a3ea136
faisal <faisalhossain7474@gmail.com>
2024-10-08 18:52:51 +0600

    first commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,101 +1,91 @@
 import Image from "next/image";
+import Hero from "../../public/img/Hero.png";
+import { FaLinkedin } from "react-icons/fa";
+import { FaSquareTwitter } from "react-icons/fa6";
+import { FaSkype } from "react-icons/fa6";
+import { FaSquareInstagram } from "react-icons/fa6";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div>
+      <table className="max-w-[600px] mx-auto bg-white border">
+        <tr>
+          <td>
+            <h1 className="text-[50px] text-[#171046] font-bold text-center pt-6">
+              Welcome!
+            </h1>
+          </td>
+        </tr>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+        <tr>
+          <td className="flex justify-center mt-6">
             <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+              alt="hero"
+              src={Hero}
+              className="object-cover w-[300px] h-[300px]"
             />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+          </td>
+        </tr>
+        <tr>
+          <td className="pt-10 flex flex-col px-5 gap-10 mb-14">
+            <p className="text-[22px] text-[#868686] text-center">
+              We're excited to have you get started! First you need to confirm
+              your account. Just click the button below.
+            </p>
+            <div className="flex justify-center">
+              <button className="bg-[#181147] text-base w-[320px] h-[56px] text-white">
+                Confirm Your Account
+              </button>
+            </div>
+            <p className="text-base text-[#868686]">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              voluptate velit esse cillum dolore eu fugiat nulla pariatur enim
+              ad minim veniam.
+            </p>
+            <p className="text-base text-[#868686]">
+              If you have any questions. Please feel free to inform - We're
+              always ready to help out.
+            </p>
+
+            <td>
+              <p className="text-lg text-[#868686]">Cheers,</p>
+              <p className="text-lg text-[#868686] font-bold">The Team Name.</p>
+            </td>
+          </td>
+        </tr>
+
+        <tr className="bg-[#181147]">
+          <td className="py-10">
+            <p className="text-4xl text-center text-white border-b-[3px] border-b-white pb-5 w-[380px] mx-auto">
+              Get In Touch
+            </p>
+            <td className="flex justify-center gap-3 mt-4 mb-5">
+              <FaLinkedin className="text-white w-[30px] h-[30px]" />
+              <FaSquareTwitter className="text-white w-[30px] h-[30px]" />
+              <FaSkype className="text-white w-[30px] h-[30px]" />
+              <FaSquareInstagram className="text-white w-[30px] h-[30px]" />
+            </td>
+            <p className="text-base text-white px-5">
+              If you have questions regarding your Data, please visit our
+              Privacy Policy
+            </p>
+            <p className="text-base text-white px-5">
+              Want to change how you receive these emails?
+            </p>
+            <p className="text-base text-white px-5">
+              You can update your preferences or unsubscribe from this list.
+            </p>
+          </td>
+        </tr>
+        <tr>
+          <td className="bg-[#f7d845]">
+            <p className="text-sm text-center py-4">
+              © 20XX Company. All Rights Reserved.
+            </p>
+          </td>
+        </tr>
+      </table>
     </div>
   );
 }
